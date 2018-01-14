26530eec9307eb1ebaee87574354872309e3d879
Steve Johnson <steve.johnson512@gmail.com>
2018-01-14 14:27:28 -0600

    Changes to db schema

--- movies.sql
+++ movies.sql
@@ -1,22 +1,35 @@
 CREATE TABLE movie(
-   id            INT PRIMARY KEY NOT NULL,
-   genre         INT,
-   actor_list_id INT,
+   id            INTEGER PRIMARY KEY NOT NULL,
+   genre_id      INTEGER,
+   actor_list_id INTEGER,
    title         CHAR(75),
-   year          INT,
-   rating        REAL
-   
-   FOREIGN KEY(actor_list_id) REFERENCES actor_list(id)
+   year          INTEGER,
+   rating        REAL,
+   FOREIGN KEY(actor_list_id) REFERENCES actor_list(id),
+   FOREIGN KEY(genre_id) REFERENCES genre(id)
 );
 
 CREATE TABLE actor_list(
-   id       INT PRIMARY KEY NOT NULL,
-   actor_id INT
+   id       INTEGER PRIMARY KEY NOT NULL,
+   actor_id INTEGER,
    FOREIGN KEY(actor_id) REFERENCES actor(id)
 );
 
 CREATE TABLE actor(
-   id         INT PRIMARY KEY NOT NULL,
-   first_name CHAR(50),
-   last_name  CHAR(50)
+   id   INTEGER PRIMARY KEY NOT NULL,
+   name CHAR(75)
 );
+
+CREATE TABLE genre(
+   id   INTEGER PRIMARY KEY NOT NULL,
+   name CHAR(25)
+);
+
+INSERT INTO genre (name)
+VALUES
+    ('Action'),
+    ('Drama'),
+    ('Comedy'),
+    ('Documentary'),
+    ('Horror'),
+    ('SciFi')
