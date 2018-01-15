166131edd816ca07a3dd3d7f7ecb236104666de7
Steve Johnson <steve.johnson512@gmail.com>
2018-01-15 10:44:05 -0600

    Edits to movies.sql

--- movies.sql
+++ movies.sql
@@ -1,18 +1,17 @@
 CREATE TABLE movie(
    id            INTEGER PRIMARY KEY NOT NULL,
    genre_id      INTEGER,
-   actor_list_id INTEGER,
    title         CHAR(75),
    year          INTEGER,
    rating        REAL,
-   FOREIGN KEY(actor_list_id) REFERENCES actor_list(id),
    FOREIGN KEY(genre_id) REFERENCES genre(id)
 );
 
-CREATE TABLE actor_list(
-   id       INTEGER PRIMARY KEY NOT NULL,
-   actor_id INTEGER,
-   FOREIGN KEY(actor_id) REFERENCES actor(id)
+CREATE TABLE cast_member(
+   movie_id INTEGER NOT NULL,
+   actor_id INTEGER NOT NULL,
+   FOREIGN KEY(actor_id) REFERENCES actor(id),
+   FOREIGN KEY(movie_id) REFERENCES movie(id)
 );
 
 CREATE TABLE actor(
@@ -25,11 +24,34 @@ CREATE TABLE genre(
    name CHAR(25)
 );
 
-INSERT INTO genre (name)
+INSERT INTO genre(name)
 VALUES
     ('Action'),
     ('Drama'),
     ('Comedy'),
     ('Documentary'),
     ('Horror'),
-    ('SciFi')
+    ('SciFi');
+	
+INSERT INTO actor(name)
+VALUES
+    ('Casey Affleck'),
+    ('Leonardo DiCaprio'),
+    ('Eddie Redmayne'),
+    ('Matthew McConaughey'),
+    ('Daniel Day-Lewis'),
+    ('Emma Stone'),
+    ('Brie Larson'),
+    ('Julianne Moore'),
+    ('Cale Blanchett'),
+    ('Jennifer Lawrence');
+	
+INSERT INTO cast_member(movie_id, actor_id)
+VALUES
+	(0,0),
+	(0,2),
+	(0,6);
+
+INSERT INTO movie (genre_id, title, year, rating)
+VALUES	
+	(2, 'My Madeup Movie', 2018, 4.7);
